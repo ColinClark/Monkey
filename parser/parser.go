80b7b9722b9840fcc095894add91dd0d43449d72
Colin Clark <colin@clark.ws>
2018-01-22 16:52:29 +0100

    parser and parser test started

---
+++ parser/parser.go
@@ -0,0 +1,38 @@
+// colin
+//
+// 1/22/18
+//
+// 11:25 AM
+
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// read two tokens so that both curToken and peekToken are set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
